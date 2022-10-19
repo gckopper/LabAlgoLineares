1885ac11f90e88a7bd61969e4ba6e6013bfe9580
Gabriel Kopper <gkopper@edu.unisinos.br>
2022-10-19 19:19:50 +0000

    Inicio e classe Paciente

---
+++ src/Paciente/Paciente.java
@@ -0,0 +1,26 @@
+package Paciente;
+
+
+public class Paciente {
+    private String nome;
+    private int idade;
+
+    public Paciente(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente nome=" + nome + ", idade=" + idade;
+    }
+    
+}
